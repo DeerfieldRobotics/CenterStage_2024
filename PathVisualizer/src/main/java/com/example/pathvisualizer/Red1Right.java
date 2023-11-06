e95510d6424ae40bb36698f0e877199f61793660
James X <xtremejames1@gmail.com>
2023-11-05 21:39:46 -0600

    improved auto pathing

--- PathVisualizer/src/main/java/com/example/pathvisualizer/Red1Right.java
+++ PathVisualizer/src/main/java/com/example/pathvisualizer/Red1Right.java
@@ -22,22 +22,24 @@ public class Red1Right {
                 //.setStartPose(new Pose2d(50, 50))
                 .setConstraints(36, 36, Math.toRadians(180), Math.toRadians(180), 13.5)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(12,-63, Math.toRadians(90)))
-                                .splineToLinearHeading(new Pose2d(7,-28, Math.toRadians(180)), Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(12,-63, Math.toRadians(90))) //initial position
+                                .splineToLinearHeading(new Pose2d(10,-40, Math.toRadians(135)), Math.toRadians(135)) //drop off purple
+                                //TODO: OUTTAKE PURPLE HERE
                                 .setTangent(Math.toRadians(0))
-                                .lineToLinearHeading(new Pose2d(50,-35,Math.toRadians(0)))
+                                .lineToLinearHeading(new Pose2d(50,-35,Math.toRadians(180))) //drop off yellow
+                                //TODO: OUTTAKE YELLOW HERE, BRING SLIDE UP AND OUTTAKE
                                 /*
                                 .back(20)
                                 .turn(Math.toRadians(180))
                                  */
-                                .lineToLinearHeading(new Pose2d(-30, -35,Math.toRadians(0)))
-                                .lineToLinearHeading(new Pose2d(-60, -35,Math.toRadians(180)))
-                                .lineToLinearHeading(new Pose2d(50, -35,Math.toRadians(0)))
-                                .back(5)
-                                .strafeLeft(10)
+//                                .lineToLinearHeading(new Pose2d(-30, -35,Math.toRadians(0)))
+                                .lineToLinearHeading(new Pose2d(-60, -35,Math.toRadians(180))) //Go back to stack
+                                //TODO: ADD INTAKE HERE
+                                .lineToLinearHeading(new Pose2d(50, -35,Math.toRadians(180))) //drop off pixel
+                                //TODO: OUTTAKE PIXEL HERE
+                                .forward(5)
+                                .strafeRight(10)
                                 .build()
-
-
                 );
         Image img = null;
         try { img = ImageIO.read(new File("./PathVisualizer/src/main/java/com/example/pathvisualizer/centerstage_bg.png")); }
