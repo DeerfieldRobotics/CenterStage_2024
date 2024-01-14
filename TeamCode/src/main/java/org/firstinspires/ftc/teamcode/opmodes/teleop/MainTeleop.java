64e571ed0d909236d110605299908b55de8a7391
James Xiao <xtremejames1@gmail.com>
2024-01-14 14:23:00 -0600

    garbage man came

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleop.java
@@ -1,16 +1,9 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.utils.hardware.Drivetrain;
 import org.firstinspires.ftc.teamcode.utils.hardware.Intake;
 import org.firstinspires.ftc.teamcode.utils.hardware.Launcher;
@@ -24,25 +17,7 @@ public class MainTeleop extends LinearOpMode {
     private Outtake outtake;
     private Slide slide;
     private Launcher launcher;
-    //Sensitivity values for triggers of gamepad 2
-    private final double rTriggerStart = 0.05;
-    private final double rTriggerEnd = 0.6;
-    private final double lTriggerStart = 0.05;
-    private final double lTriggerEnd = 0.6;
 
-    //stick sensitivity values
-    private final double l1Sensitivity = 1;
-    private final double r1Sensitivity = 1;
-    private final double l2Sensitivity = 1;
-    private final double r2Sensitivity = 1;
-    private double l2Max = 0.8;
-    //driving values
-    private double speedMult;
-    private double forwardMult = 1;
-    private double turnMult = .75;
-    private double strafeMult = 1;
-
-//    SampleMecanumDrive drive;
     private boolean crossToggle = false;
     private boolean triangleToggle = false;
     private boolean squareToggle = false;
@@ -50,24 +25,19 @@ public class MainTeleop extends LinearOpMode {
     private boolean rightBumperToggle = false;
     private boolean leftBumperToggle = false;
     private boolean dpadLeftToggle = false;
-
-    private boolean runToPos = false;
     private double lastTickTime = 0;
     private double avgTickTime = 0;
     private int tickCount = 0;
-    private ElapsedTime runtime = new ElapsedTime();
-
-    private IMU imu;
-    // TODO fix parameters for c-hub placement
-    private IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-            RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
-            RevHubOrientationOnRobot.UsbFacingDirection.UP
-    ));
 
     private void driveNormal() {
-        speedMult = .7+0.3 * gamepad1.right_trigger-0.3*gamepad1.left_trigger;
+        //driving values
+        double speedMult = .7 + 0.3 * gamepad1.right_trigger - 0.3 * gamepad1.left_trigger;
 
         gamepad1.rumble(gamepad1.left_trigger>0.5?(gamepad1.left_trigger-0.5)/.4:0.0,gamepad1.right_trigger>0.4?(gamepad1.right_trigger-0.4)/0.8:0.0,50);
+
+        double forwardMult = 1;
+        double turnMult = .75;
+        double strafeMult = 1;
 
         double forward = gamepad1.left_stick_y * forwardMult * speedMult;
         double turn = -gamepad1.right_stick_x * turnMult * speedMult;
@@ -75,84 +45,22 @@ public class MainTeleop extends LinearOpMode {
 
         drivetrain.move(forward, strafe, turn);
     }
-    //mack poop
-    private void driveCentered() {
-        double y = -gamepad1.left_stick_y;
-        double x = gamepad1.left_stick_x;
-        double rx = gamepad1.right_stick_x;
-
-        if (gamepad1.options) { imu.resetYaw(); }
-
-        double h = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-
-        telemetry.addData("IMU Heading", Math.toDegrees(h));
-
-        x = x * Math.cos(-h) - y * Math.sin(-h);
-        y = x * Math.sin(-h) + y * Math.cos(-h);
-
-        double flp = y + x + rx;
-        double blp = y - x + rx;
-        double frp = y - x - rx;
-        double brp = y + x - rx;
-
-        drivetrain.setMotorPower(flp, frp, blp, brp);
-    }
-
-    //kevin good
-//    private void driveRRCentered(){
-//        Pose2d poseEstimate = drive.getPoseEstimate();
-//
-//        // Create a vector from the gamepad x/y inputs
-//        // Then, rotate that vector by the inverse of that heading
-//        Vector2d input = new Vector2d(
-//                -0.8*gamepad1.left_stick_y,
-//                -0.8*gamepad1.left_stick_x
-//        ).rotated(-poseEstimate.getHeading());
-//
-//        // Pass in the rotated input + right stick value for rotation
-//        // Rotation is not part of the rotated input thus must be passed in separately
-//        drive.setWeightedDrivePower(
-//                new Pose2d(
-//                        input.getX(),
-//                        input.getY(),
-//                        0.6*gamepad1.right_stick_x
-//                )
-//        );
-//
-//        if(gamepad1.ps){
-//            drive.setPoseEstimate(new Pose2d(0,0,Math.toRadians(90)));
-//        }
-//
-//        // Update everything. Odometry. Etc.
-//        drive.update();
-//
-//        // Print pose to telemetry
-//        telemetry.addData("x", poseEstimate.getX());
-//        telemetry.addData("y", poseEstimate.getY());
-//        telemetry.addData("heading", poseEstimate.getHeading());
-//        telemetry.update();
-//    }
 
     @Override
     public void runOpMode() {
         initialize();
 
         waitForStart();
-        runtime = new ElapsedTime();
+        ElapsedTime runtime = new ElapsedTime();
 
         while (opModeIsActive()) {
             tickCount++;
             telemetry.addData("Tick Time", runtime.milliseconds()-lastTickTime);
             lastTickTime = runtime.milliseconds();
-            avgTickTime = (avgTickTime*(tickCount-1)+runtime.milliseconds()-lastTickTime)/tickCount;
+            avgTickTime = (avgTickTime*(tickCount-1)+ runtime.milliseconds()-lastTickTime)/tickCount;
             telemetry.addData("Avg Tick Time", avgTickTime);
 
-//            if (gamepad1.ps) {
-//                imu.resetYaw();
-//            }
-//            driveCentered();
             driveNormal();
-//            driveRRCentered();
 
             slide();
             intake();
@@ -175,13 +83,25 @@ public class MainTeleop extends LinearOpMode {
     }
     // In MainTeleop.java
     private void slide() {
-        slide.setPower(gamepad2.left_stick_y*l2Sensitivity/l2Max);
+        //stick sensitivity values
+        double l2Sensitivity = 1;
+        double l2Max = 0.8;
+        slide.setPower(gamepad2.left_stick_y* l2Sensitivity / l2Max);
     }
     private void intake() {
         //TODO: add vibration feedback when intake is fully opened and closed
         //Set intake values and clamp them between 0 and 1
-        double rightTrigger = Math.max((gamepad2.right_trigger-rTriggerStart)/(rTriggerEnd-rTriggerStart),0);
-        double leftTrigger = Math.max((gamepad2.left_trigger-lTriggerStart)/(lTriggerEnd-lTriggerStart),0);
+        //Sensitivity values for triggers of gamepad 2
+        double rTriggerStart = 0.05;
+        double rTriggerEnd = 0.6;
+
+        double rightTrigger = Math.max((gamepad2.right_trigger- rTriggerStart)/(rTriggerEnd - rTriggerStart),0);
+
+        double lTriggerStart = 0.05;
+        double lTriggerEnd = 0.6;
+
+        double leftTrigger = Math.max((gamepad2.left_trigger- lTriggerStart)/(lTriggerEnd - lTriggerStart),0);
+
         intake.intake(0.8*(rightTrigger-leftTrigger));
 
         //Outtake Code
@@ -191,16 +111,6 @@ public class MainTeleop extends LinearOpMode {
             intake.changeIntakeServo(.5);
         if(gamepad2.dpad_down)
             intake.changeIntakeServo(-.5);
-
-//        if(gamepad2.dpad_left && !dpadLeftToggle) {
-//            dpadLeftToggle = true;
-//            slide.threadKill();
-//            outtake.threadKill();
-//            intake.threadKill();
-//        }
-//        if(!gamepad2.dpad_left) {
-//            dpadLeftToggle = false;
-//        }
 
         if(gamepad1.right_trigger>0.3 && intake.getServoPosition()== Intake.IntakePositions.INTAKE)
             intake.setServoPosition(Intake.IntakePositions.DRIVE);
@@ -274,33 +184,15 @@ public class MainTeleop extends LinearOpMode {
             launcher.load();
     }
 
-
     public void initialize() {
-        imu = hardwareMap.get(IMU.class, "imu");
-        imu.initialize(parameters);
-
-//        drive = new SampleMecanumDrive(hardwareMap);
-
         slide = new Slide(hardwareMap);
         outtake = new Outtake(hardwareMap, slide);
         drivetrain = new Drivetrain(hardwareMap);
         intake = new Intake(hardwareMap);
         launcher = new Launcher(hardwareMap);
 
-        //slide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         drivetrain.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        // We want to turn off velocity control for teleop
-        // Velocity control per wheel is not necessary outside of motion profiled auto
-//        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        // Retrieve our pose from the PoseStorage.currentPose static field
-        // See AutoTransferPose.java for further details
-//        drive.setPoseEstimate(new Pose2d(0,0,Math.toRadians(90)));
-
-//        drive.reverseMotors();
 
         waitForStart();
     }
-
 }
