a51d2e792df0ae518ad6eb19ef47969f88aa4752
James Xiao <xtremejames1@gmail.com>
2024-01-15 13:49:20 -0600

    added datalog to AprilTagAlignmentTester.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testers/AprilTagAlignmentTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testers/AprilTagAlignmentTester.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.utils.Other.Datalogger;
 import org.firstinspires.ftc.teamcode.utils.detection.AprilTagAlignment;
 import org.firstinspires.ftc.teamcode.utils.hardware.Drivetrain;
 
@@ -16,11 +17,13 @@ public class AprilTagAlignmentTester extends LinearOpMode {
     private Drivetrain drivetrain;
     private WebcamName webcam;
     private AprilTagAlignment aprilTagAlignment;
+    private Datalog datalog;
     private boolean dpadLeftToggle = false;
     private boolean dpadRightToggle = false;
 
     @Override
     public void runOpMode() throws InterruptedException {
+        datalog = new Datalog("AprilTagAlignmentTester");
 
         drivetrain = new Drivetrain(hardwareMap);
         webcam = hardwareMap.get(WebcamName.class, "Webcam 1");
@@ -62,6 +65,10 @@ public class AprilTagAlignmentTester extends LinearOpMode {
             telemetry.addData("heading error","%3.0f degrees", aprilTagAlignment.getHeadingError());
             telemetry.addData("drivetrain power", Collections.max(Arrays.asList(drivetrain.getMotorPower())));
             telemetry.update();
+
+            datalog.xError.set(aprilTagAlignment.getXError());
+            datalog.yError.set(aprilTagAlignment.getYError());
+            datalog.headingError.set(aprilTagAlignment.getHeadingError());
         }
     }
     private void driveNormal() {
@@ -78,6 +85,46 @@ public class AprilTagAlignmentTester extends LinearOpMode {
         double strafe = -gamepad1.left_stick_x * strafeMult * speedMult;
 
         drivetrain.move(forward, strafe, turn);
+    }
+    public static class Datalog
+    {
+        // The underlying datalogger object - it cares only about an array of loggable fields
+        private final Datalogger datalogger;
+
+        // These are all of the fields that we want in the datalog.
+        // Note that order here is NOT important. The order is important in the setFields() call below
+        public Datalogger.GenericField xError       = new Datalogger.GenericField("X Error");
+        public Datalogger.GenericField yError       = new Datalogger.GenericField("Y Error");
+        public Datalogger.GenericField headingError = new Datalogger.GenericField("Heading Error");
+
+        public Datalog(String name)
+        {
+            // Build the underlying datalog object
+            datalogger = new Datalogger.Builder()
+
+                    // Pass through the filename
+                    .setFilename(name)
+
+                    // Request an automatic timestamp field
+                    .setAutoTimestamp(Datalogger.AutoTimestamp.DECIMAL_SECONDS)
+
+                    // Tell it about the fields we care to log.
+                    // Note that order *IS* important here! The order in which we list
+                    // the fields is the order in which they will appear in the log.
+                    .setFields(
+                            xError,
+                            yError,
+                            headingError
+                    )
+                    .build();
+        }
+
+        // Tell the datalogger to gather the values of the fields
+        // and write a new line in the log.
+        public void writeLine()
+        {
+            datalogger.writeLine();
+        }
     }
 }
 
