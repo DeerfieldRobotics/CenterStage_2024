89dc818d8212ddd908fc8b5a57ec6af69f0827d2
tonythedrony <aretelew@gmail.com>
2024-01-28 11:16:04 -0600

    Fixed Cycle Paths & Potential RedFarLeft Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/PoseHelper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/PoseHelper.java
@@ -33,9 +33,9 @@ public final class PoseHelper {
     public final static Pose2d initFarBlue = new Pose2d(-33, 63, Math.toRadians(270));
     public final static Pose2d backboardLeftBlue = new Pose2d(48, 40, Math.toRadians(180.0));
     public final static Pose2d backboardCenterBlue = new Pose2d(48, 34, Math.toRadians(180.0));
-    public final static Pose2d backboardRightBlue = new Pose2d(48, 29, Math.toRadians(180.0));
+    public final static Pose2d backboardRightBlue = new Pose2d(48, 28.5, Math.toRadians(180.0));
     public final static Pose2d farSpikeRightBlue = new Pose2d(-46.5, 38.5, Math.toRadians(-90.0));
-    public final static Pose2d farSpikeCenterBlue = new Pose2d(-49.5,37, Math.toRadians(-90.0));
+    public final static Pose2d farSpikeCenterBlue = new Pose2d(-39.5,37, Math.toRadians(-90.0));
     public final static Pose2d farSpikeLeftBlue = new Pose2d(-30.5, 39, Math.toRadians(-50.0));
     public final static Pose2d closeSpikeRightBlue = new Pose2d(10.5, 30, Math.toRadians(180));
     public final static Pose2d closeSpikeCenterBlue = new Pose2d(20.5, 24, Math.toRadians(180));
@@ -45,8 +45,8 @@ public final class PoseHelper {
     public final static Pose2d insideStackBlue = new Pose2d(-54.5, 12, Math.toRadians(180.0));
     public final static Pose2d wingTrussOutsideBlue = new Pose2d(-35, 57, Math.toRadians(180.0));
     public final static Pose2d boardTrussOutsideBlue = new Pose2d(8, 57, Math.toRadians(180.0));
-    public final static Pose2d wingTrussInsideBlue = new Pose2d(-33, 11, Math.toRadians(180.0));
-    public final static Pose2d boardTrussInsideBlue = new Pose2d(17, 11, Math.toRadians(180.0));
+    public final static Pose2d wingTrussInsideBlue = new Pose2d(-33, 9, Math.toRadians(180.0));
+    public final static Pose2d boardTrussInsideBlue = new Pose2d(17, 9, Math.toRadians(180.0));
     public final static Pose2d parkPoseInsideBlue = new Pose2d(58.5, 11, Math.toRadians(180.0));
     public final static Pose2d parkPoseOutsideBlue = new Pose2d(58.5, 59, Math.toRadians(180.0));;
 //
